12101e7534797bdaca9713ba3da7841fe20087ff
Rafael Gervasio <rafaelgaspargervasio@gmail.com>
2023-11-29 19:41:54 +0000

    Second commit. Added index, new articles

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  root "articles#test"
+  root "articles#index"
+
+  resources :articles
 
   get "/articles", to: "articles#index"
+  get "/articles/:id", to: "articles#show"
 end
